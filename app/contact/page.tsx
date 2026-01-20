dff4c2f988e9b1b6e05ae26ac710b877827d1c95
Matt Gomez <matthewgomezm10@gmail.com>
2026-01-20 14:53:52 -0500

    v1

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+export default function Contact() {
+    return (
+      <main>
+        <section className="contact-section">
+          <h2>Get in touch with me!</h2>
+          <p>If you'd like to book, please get in contact with me below.</p>
+          <ul className="contact-info">
+            <li>Email: janetzhangmedia@gmail.com</li>
+            <li>Phone: (972) 202 6768</li>
+          </ul>
+        </section>
+      </main>
+    );
+  }
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,40 @@ import fs from 'fs';
 import path from 'path';
 import Image from 'next/image';
 
-async functio getImages(): Promise<string[]> {
-  const galleryDir = path.jion(process.cwd(), 'public/gallery');
-
-  if(!fs.existsSync(galleryDir)) {
+async function getGalleryImages(): Promise<string[]> {
+  const galleryDir = path.join(process.cwd(), 'public/gallery');
+  
+  if (!fs.existsSync(galleryDir)) {
     return [];
   }
 
-  const files = fs.readdirSync(galleryDir);
-  const images = filenames.filter
+  const filenames = fs.readdirSync(galleryDir);
+  const images = filenames.filter((file) => 
+    /\.(jpeg|jpg|png|webp)$/i.test(file)
+  );
+
+  return images.map(name => `/gallery/${name}`);
+}
+
+export default async function Home() {
+  const images = await getGalleryImages();
+
+  return (
+    <main>
+      <section className="gallery">
+        {images.map((src, index) => (
+          <div key={index} className="gallery-item">
+            <Image 
+              src={src} 
+              alt={`Portfolio shot ${index+1}`} 
+              width={500} 
+              height={500} 
+              className="gallery-img"
+              style={{ width:'100%', height:'100%', objectFit:'cover' }}
+            />
+          </div>
+        ))}
+      </section>
+    </main>
+  );
+}
